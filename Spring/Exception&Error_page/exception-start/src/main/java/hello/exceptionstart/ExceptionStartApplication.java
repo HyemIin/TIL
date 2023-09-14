924da84f63d3230d95e906fd87a0c60cc84c0951
Harry <114489245+HyemIin@users.noreply.github.com>
2023-09-14 21:52:18 +0900

    Add files via upload

---
+++ Spring/Exception&Error_page/exception-start/src/main/java/hello/exceptionstart/ExceptionStartApplication.java
@@ -0,0 +1,13 @@
+package hello.exceptionstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionStartApplication.class, args);
+	}
+
+}
---
+++ Spring/Exception&Error_page/exception-start/src/test/java/hello/exceptionstart/ExceptionStartApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.exceptionstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
